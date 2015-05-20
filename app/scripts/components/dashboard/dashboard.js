96d0770c53f5a6455d906634ac6c80b4561f13b9
Thawon Uttamavanit <uttamavanit@gmail.com>
2015-05-21 01:23:17 +1000

    implement save dashboard

--- app/scripts/components/dashboard/dashboard.js
+++ app/scripts/components/dashboard/dashboard.js
@@ -11,17 +11,44 @@
     .controller('DashboardController', DashboardController);
 
     DashboardController.$inject = [
+        '$q',
         'VIEW_MODES',
         'ClientService'
     ];
 
-    function DashboardController(VIEW_MODES, ClientService) {
+    function DashboardController($q, VIEW_MODES, ClientService) {
         var that = this;
         
         that.mode = mode;
 
+        that.validate = validate;
+
+        that.cancel = cancel;
+
+        that.save = save;;
+
         function mode() {
             return VIEW_MODES.show;
+        }
+
+        function validate() {
+            var errors = [],
+                result = {
+                    hasError: (errors.length > 0) ? true : false,
+                    Errors: _.pluck(errors, 'errorMessage')
+                };
+
+            return result;
+        }
+
+        function cancel() { }
+
+        function save() {
+            var deferred = $q.defer();
+
+            deferred.resolve(null);
+
+            return deferred.promise;
         }
 
         that.data = ClientService.getClients();
